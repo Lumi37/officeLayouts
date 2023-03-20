ef40f5347614a3ef70b19fe3d1cb011df71c7ce6
kostas <eoppep@eoppep.gr>
2023-03-20 11:18:29 +0200

    fixes

--- src/client/javascript/index.js
+++ src/client/javascript/index.js
@@ -27,22 +27,22 @@ let selectedUser;
 let hoverUser
 let selectListedUser
 
-
 window.addEventListener('resize',()=>{
-  const svg = document.querySelector('.chosenOffice')
-  // const rectContainer = document.querySelector('.chosenOffice .rectContainer')
-  // const rect = document.querySelector('.chosenOffice .rectUser')
-  const windowWidth = window.innerWidth
-  const windowHeight = window.innerHeight
-  if(windowHeight>801)
-    svg.style.height = '800px'
-  else
-  svg.style.height = String(windowHeight - 167)+'px'
-  if(windowWidth>1201)
-    svg.style.width = '1200px'
-  else
-  svg.style.width = String(windowWidth - 274)+'px'
-
+  if(document.querySelector('.chosenOffice')){
+    const svg = document.querySelector('.chosenOffice')
+    const windowWidth = window.innerWidth
+    const windowHeight = window.innerHeight
+    if(windowHeight>801)
+      svg.style.height = '800px'
+    else
+      svg.style.height = String(windowHeight - 167)+'px'
+    if(windowWidth>1201)
+      svg.style.width = '1200px'
+    else
+      svg.style.width = String(windowWidth - 274)+'px'
+  
+  }
+  
 })
 
 svgContainer.addEventListener("click", (e) => {
