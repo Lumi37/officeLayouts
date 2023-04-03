f16f548e9122a1e8fe370e9a96d87fe5b25b2fcc
kostas <eoppep@eoppep.gr>
2023-04-03 11:43:09 +0300

    resolved issues

--- src/server/modules/readFile.js
+++ src/server/modules/readFile.js
@@ -3,7 +3,7 @@ import {_dirname} from '../server.js'
 export async function readToFile(office){
     let  requestedFile = `${_dirname}/../../content/offices/${office}.json`
     try{    
-        content = await fs.readFile(requestedFile,{encoding: 'utf-8'})
+        const content = await fs.readFile(requestedFile,{encoding: 'utf-8'})
         return content
     }catch(err){
         console.log(err)
