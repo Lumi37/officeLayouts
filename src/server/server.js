3167b5681084183b3ff9d4792489aadcddcf1087
kostas <eoppep@eoppep.gr>
2023-04-07 18:12:50 +0300

    wer

--- src/server/server.js
+++ src/server/server.js
@@ -11,14 +11,18 @@ export const _dirname = new URL('.',import.meta.url).pathname
 server.use(express.static(`${_dirname}/../client/`));
 server.use(express.json());
 server.use(r)
+console.log('1')
 initIdapUsers(await allStoredUsers())
 server.listen(3000, console.log("listens to 3000"));
 
 async function allStoredUsers(){
-    const offices = await Promise.all(
+        console.log('2')
+        const offices = await Promise.all(
             (await fs.readdir(`${_dirname}/../../content/offices/`))
             .map( async office => 
                 JSON.parse(await fs.readFile(`${_dirname}/../../content/offices/${office}`,'utf-8')))  
         )
+        console.log('3')
+
         return offices.flat()
 }
