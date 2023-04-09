e190f8b5e8d9c9bf08783dd9b31953226a3c0447
Lumi <37skmg@gmail.com>
2023-04-09 16:38:17 +0300

    removed console.logs

--- src/server/server.js
+++ src/server/server.js
@@ -11,18 +11,15 @@ export const _dirname = new URL('.',import.meta.url).pathname
 server.use(express.static(`${_dirname}/../client/`));
 server.use(express.json());
 server.use(r)
-console.log('1')
 initIdapUsers(await allStoredUsers())
 server.listen(3000, console.log("listens to 3000"));
 
 export async function allStoredUsers(){
-        console.log('2')
         const offices = await Promise.all(
             (await fs.readdir(`${_dirname}/../../content/offices/`))
             .map( async office => 
                 JSON.parse(await fs.readFile(`${_dirname}/../../content/offices/${office}`,'utf-8')))  
         )
-        console.log('3')
 
         return offices.flat()
 }
